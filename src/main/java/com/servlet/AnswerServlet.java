2917c929d4a2d2b526a8b7d56311c2df52ebf8f8
wxw-429 <3092220295@qq.com>
2020-05-29 20:51:57 +0800

    完成了AnswerServlet及GetTreeServlet,暂未调试

--- src/main/java/com/servlet/AnswerServlet.java
+++ src/main/java/com/servlet/AnswerServlet.java
@@ -1,17 +1,46 @@
 package com.servlet;
 
+import com.alibaba.fastjson.JSON;
+import com.entities.Answer;
+import com.service.AnswerPasser;
+import com.service.AnswerPasserImpl;
+import org.apache.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
 
 @WebServlet(name = "AnswerServlet",urlPatterns = "/answer")
 public class AnswerServlet extends HttpServlet {
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //设置输入格式为UTF-8
+        resp.setCharacterEncoding("UTF-8");
+        //设置内容类型为UTF-8
+        resp.setContentType("application/json;charset=utf-8");
+        //获得打印流对象
+        PrintWriter out = resp.getWriter();
+        //此打印流对象可向浏览器输出信息
+        AnswerPasserImpl answerPasser=AnswerPasserImpl.getInstance();
+        Answer answer=null;
+        try {
+            answer =answerPasser.passAnswer();
+        } catch (SQLException throwables) {
+            //控制台输出
+            throwables.printStackTrace();
+        }
+        //输出answer
+        System.out.println(answer);
+        //向外部写出
+        System.out.println(JSON.toJSONString(answer));
+        out.write(JSON.toJSONString(answer));
+        out.flush();
 
-        super.service(req, resp);
     }
 }
--- src/main/java/com/servlet/GetTreeServlet.java
+++ src/main/java/com/servlet/GetTreeServlet.java
@@ -1,17 +1,46 @@
 package com.servlet;
 
+import com.alibaba.fastjson.JSON;
+import com.entities.Tree;
+import com.service.TreePasser;
+import com.service.TreePasserImpl;
+import org.apache.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
 
 @WebServlet(name = "GetTreeServlet",urlPatterns = "/getTree")
 public class GetTreeServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //设置输入格式为UTF-8
+        resp.setCharacterEncoding("UTF-8");
+        //设置内容类型为UTF-8
+        resp.setContentType("application/json;charset=utf-8");
+        //获得打印流对象
+        PrintWriter out = resp.getWriter();
+        //此打印流对象可向浏览器输出信息
+        TreePasserImpl treePasser=TreePasserImpl.getInstance();
+        Tree tree=null;
+        try {
+            tree =treePasser.getRandomTree();
+        } catch (SQLException throwables) {
+            //控制台输出
+            throwables.printStackTrace();
+        }
+        //输出tree
+        System.out.println(tree);
+        //向外部写出
+        System.out.println(JSON.toJSONString(tree));
+        out.write(JSON.toJSONString(tree));
+        out.flush();
 
-        super.service(req, resp);
+
     }
 }
